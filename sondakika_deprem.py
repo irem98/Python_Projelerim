5fb49052922e3e2c7b1521ee34caa3814acbe671
irem98 <66281358+irem98@users.noreply.github.com>
2023-03-25 00:09:58 +0300

    Add files via upload

---
+++ sondakika_deprem.py
@@ -0,0 +1,14 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "http://www.koeri.boun.edu.tr/scripts/lst9.asp"
+
+response = requests.get(url)
+content = response.content
+
+soup = BeautifulSoup(content,"html.parser")
+pre=soup.find("pre")
+text = pre.text
+liste = text.split("\n")
+
+print("\n".join(liste[0:500]))
