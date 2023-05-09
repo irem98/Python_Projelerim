7eeda34a949878f4b28866c5135bac864d5c53a3
irem98 <66281358+irem98@users.noreply.github.com>
2023-05-09 11:05:35 +0300

    Add files via upload

---
+++ math_pi.py
@@ -0,0 +1,11 @@
+import math
+
+# print(math.pi)
+
+r = float(input("Dairenin yarıçapını giriniz:"))
+cevre = 2 * math.pi * r
+print("Dairenin çevresi",cevre)
+
+alan = math.pi * (r**2)
+print("Dairenin alanı",alan)
+
